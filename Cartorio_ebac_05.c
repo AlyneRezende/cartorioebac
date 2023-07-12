72e351d405ae87565f82f901445dd3536765fd0d
Alyne Rezende <115512180+AlyneRezende@users.noreply.github.com>
2023-07-12 22:44:36 +0100

    Upload do projeto
    Projeto completo de acordo com as especificações do cliente.

---
+++ Cartorio_ebac_05.c
@@ -0,0 +1,163 @@
+#include <stdio.h> //biblioteca de comunicação com o usuário
+#include <stdlib.h> //biblioteca de alocação de espaço em memória
+#include <locale.h> //biblioteca de alocações de texto por região
+#include <string.h> // biblioteca responsável por cuidar das string
+
+// \n é para saltar linhas
+// \t funciona como espaçamento no ínicio do texto
+// = atribuição e == comparação != diferença
+
+int registrar() // não tem ; porque é uma função.
+{
+	//inicio de criação das variáveis/string (string é um conjunto de váriavel)
+	char arquivo[40];
+	char cpf[40]; // char tipo string
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	//final da criação das variáveis/string
+	
+	printf("Digite o CPF a ser cadastrado: ");
+	scanf("%s",cpf); // salvar dentro da string - %s refere-se a string
+	
+	strcpy(arquivo, cpf); // responsável por copiar os valores das string, ou seja, ta criando o nome do arquivo igual ao cpf
+	
+	FILE *file; // cria o arquivo no banco de dados
+	file = fopen(arquivo, "w"); // abre a variável arquivo e escreva (w, write) o que tem lá
+	fprintf(file,cpf); // salvo o valor da variável
+	fclose(file); // fecha o arquivo
+	
+	file = fopen(arquivo, "a"); // abre o arquivo já existente e salva (a, append) mais dados dentro do mesmo arquivo
+	fprintf(file,"-");
+	fclose(file);
+	
+	printf("Digite o nome a ser cadastrado: ");
+	scanf("%s",nome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,nome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a"); // abre o arquivo já existente e salva (a, append) mais dados dentro do mesmo arquivo
+	fprintf(file, "-");
+	fclose(file);
+	
+	printf("Digite o sobrenome a ser cadastrado: ");
+	scanf("%s",sobrenome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,sobrenome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a"); // abre o arquivo já existente e salva (a, append) mais dados dentro do mesmo arquivo
+	fprintf(file,"-");
+	fclose(file);
+	
+	printf("Digite o cargo a ser cadastrado: ");
+	scanf("%s",cargo);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,cargo);
+	fclose(file);	
+	
+	system("pause");
+}
+
+int consultar()
+{
+	setlocale(LC_ALL, "Portuguese");
+	
+	char cpf[40];
+	char conteudo[200];
+	
+	printf("Digite o cpf a ser consultado: ");
+	scanf("%s",cpf);
+	
+	FILE *file;
+	file = fopen(cpf, "r");
+	
+	if(file == NULL)
+	{
+		printf("Não foi possível abrir o arquivo, não localizado!\n");
+	}
+	
+	while(fgets(conteudo, 200, file) != NULL)
+	{
+		printf("\nEssas são as informações do usuário: ");
+		printf("%s", conteudo);
+		printf("\n\n");
+	}
+	
+	system("pause");
+}
+
+int deletar()
+{
+	char cpf[40];
+	
+	printf("Digite o CPF a ser deletado: ");
+	scanf("%s",cpf); // salvar dentro da variavel cpf
+	
+	remove(cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	fclose(file);
+	
+	if(file == NULL)
+	{
+		printf("O usuário não se encontra no sistema!\n");
+		system("pause");
+	}
+}
+
+
+int main()
+{
+	int opcao=0;  // = atribuido
+	int laco=1;
+	
+		for(laco=1;laco=1;)
+		{
+		
+			system("cls");
+		
+			setlocale(LC_ALL, "Portuguese");
+	
+			printf("\t### Cartório da EBAC ###\n\n"); // início do menu
+			printf("Escolha a opção desejada do menu:\n\n");
+			printf("\t1 -  Registrar nomes\n");
+			printf("\t2 -  Consultar nomes\n");
+			printf("\t3 -  Deletar nomes\n\n");
+			printf("Opção:"); // fim do menu
+	
+			scanf("%d", &opcao); // %d armazenar a variável tipo inteiro (armazenando a escolha do usuário)
+	
+			system("cls"); // função de deletar as mensagens anteriores
+		
+			switch(opcao) // inicio da selecao do menu
+			{
+				case 1:
+					registrar(); // chamada de funções
+					break;
+				
+				case 2:
+					consultar();
+					break;
+				
+				case 3:
+					deletar();
+					break;
+			
+				default:
+					printf("Essa opção não está disponível!\n\n");
+					system("pause");
+					break;
+					// fim da seleção
+		
+		}
+	}
+	
+	printf("Esse software é de livre uso dos alunos\n");
+	
+}
